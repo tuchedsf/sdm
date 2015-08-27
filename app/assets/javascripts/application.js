08e0aab93a58e00fb666373d104746149058c4a4
Diego <tuchedsf@gmail.com>
2015-08-26 22:59:53 -0300

    exemplos hide collum about/calleds and aplication.js, antes da limpeza

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,8 +18,8 @@
 $(document).on('ready page:load',function(){
   $(function() {
 
-    $('table.display').DataTable({
-        "responsive": true,
+    $('#myTable').DataTable({
+        //"responsive": true,
         "language": {
             "info": "Exibindo página _PAGE_ de _PAGES_" ,
             "infoEmpty": "Nenhum registro encontrado",
@@ -33,33 +33,115 @@ $(document).on('ready page:load',function(){
                 "next": "Próxima",
                 "previous": "Anterior"
                 }
+        },
+        responsive: {
+        //breakpoints: [
+        //    { name: 'desktop', width: 480 }]
         }
-      });
+    });
 
 
-
-    $('#example').DataTable( {
+    $('#teste').DataTable( {
         responsive: {
             details: {
-                type: 'column',
-                target: -1
+                renderer: function ( api, rowIdx ) {
+                    // Select hidden columns for the given row
+                    var data = api.cells( rowIdx, ':hidden' ).eq(0).map( function ( cell ) {
+                        var header = $( api.column( cell.column ).header() );
+
+                        return '<tr>'+
+                                '<td>'+
+                                    header.text()+':'+
+                                '</td> '+
+                                '<td>'+
+                                    api.cell( cell ).data()+
+                                '</td>'+
+                            '</tr>';
+                    } ).toArray().join('');
+
+                    return data ?
+                        $('<table/>').append( data ) :
+                        false;
+                }
             }
-        },
-        columnDefs: [ {
-            className: 'control',
-            orderable: false,
-            targets:   -1
-        } ]
+        }
+    } );
+
     } );
 
 
 
+/* Formating function for row details */
+function fnFormatDetails (oTable, nTr )
+{
+    var aData = oTable.fnGetData( nTr );
+    var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+    sOut += '<tr><td>Rendering engine:</td><td>' +aData[10]+ '</td></tr>';
+    sOut += '<tr><td>Link to source:</td><td>Could provide a link here</td></tr>';
+    sOut += '<tr><td>Extra info:</td><td>And any further details here (images etc)</td></tr>';
+    sOut += '</table>';
+
+    return sOut;
+}
+
+$(document).ready(function() {
 
 
 
 
+    /*
+     * Insert a 'details' column to the table
+     */
+    var nCloneTh = document.createElement( 'th' );
+    var nCloneTd = document.createElement( 'td' );
+    nCloneTd.innerHTML = '<img src="../examples_support/details_open.png">';
+    nCloneTd.className = "center";
 
+    $('#example thead tr').each( function () {
+        this.insertBefore( nCloneTh, this.childNodes[0] );
     } );
+
+    $('#example tbody tr').each( function () {
+        this.insertBefore(  nCloneTd.cloneNode( true ), this.childNodes[0] );
+    } );
+
+    /*
+     * Initialse DataTables, with no sorting on the 'details' column
+     */
+    var oTable = $('#example').dataTable( {
+        "aoColumnDefs": [
+            { "bSortable": false, "aTargets": [ 0 ] },{ "bVisible": false, "aTargets": [10] }
+        ],
+        "aaSorting": [[1, 'asc']]
+
+    });
+
+    /* Add event listener for opening and closing details
+     * Note that the indicator for showing which row is open is not controlled by DataTables,
+     * rather it is done here
+     */
+
+      $('#example tbody td').on('click', 'img' ,function () {
+
+        var nTr = $(this).parents('tr')[0];
+        if ( oTable.fnIsOpen(nTr) )
+        {
+            /* This row is already open - close it */
+            this.src = "../examples_support/details_open.png";
+            oTable.fnClose( nTr );
+        }
+        else
+        {
+            /* Open this row */
+            this.src = "../examples_support/details_close.png";
+            oTable.fnOpen( nTr, fnFormatDetails(oTable, nTr), 'details' );
+        }
+    } );
+} );
+
+
+
+
 
 
 });
