ffe61fc48d016b4510745f9b1b020bd5309be9ff
Diego <tuchedsf@gmail.com>
2015-09-04 13:55:54 -0300

    ajustes layout bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,19 @@ module ApplicationHelper
     end
   end
 
+
+   def formata_SimNao(var)
+    Rails.logger.debug "DEBUG: params are #{var}"
+
+    if !var.nil?
+      if var == true
+        var = "Sim"
+      else
+        var = "Não"
+      end
+    else
+      var = "--"
+    end
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,10 @@ class Category < ActiveRecord::Base
 
   belongs_to :system
   has_many :calleds
-  validates_presence_of :descricao, :ativo, :system_id
+  validates_presence_of :descricao, :system_id
+  #validates :ativo, :inclusion => {:in => [true, false]}
+
+  #  validates :ativo,
+   #         :presence => { :if => 'ativo.nil?' }
 
 end
